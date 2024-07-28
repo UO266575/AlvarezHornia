8c4dd0db8f2d67d78508e03ccb371f2bf5310ce8
tonpm14 <uo266575@uniovi.es>
2024-07-28 19:35:50 +0200

    Miscelanea

--- src/app/pages/photo/miscelanea/miscelanea.component.ts
+++ src/app/pages/photo/miscelanea/miscelanea.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { CarouselComponent } from '../../../components/carousel/carousel.component';
 
 @Component({
   selector: 'app-miscelanea',
   standalone: true,
-  imports: [],
+  imports: [ CarouselComponent,],
   templateUrl: './miscelanea.component.html',
   styleUrl: './miscelanea.component.scss'
 })
 export class MiscelaneaComponent {
 
+  firstCarouselInterval = 4000;
+  secondCarouselInterval = 9000;
+  thirdCarouselInterval = 7000;
+
+  ngOnInit() {
+    this.updateIntervals();
+  }
+
+  updateIntervals() {
+    setInterval(() => {
+      this.firstCarouselInterval = this.getRandomInterval();
+      this.secondCarouselInterval = this.getRandomInterval();
+      this.thirdCarouselInterval = this.getRandomInterval();
+    }, this.getMaxInterval());
+  }
+
+  getRandomInterval() {
+    return Math.floor(Math.random() * (10000 - 6000 + 1)) + 6000;
+  }
+
+  getMaxInterval() {
+    return Math.max(this.firstCarouselInterval, this.secondCarouselInterval, this.thirdCarouselInterval);
+  }
 }
